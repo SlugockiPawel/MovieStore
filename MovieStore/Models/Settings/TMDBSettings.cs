d48b0e183b12ff2c2d1039191b660135fd3860aa
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-22 20:19:17 +0100

    add models that mimics appsettings JSON structure

--- MovieStore/Models/Settings/TMDBSettings.cs
+++ MovieStore/Models/Settings/TMDBSettings.cs
@@ -2,6 +2,16 @@
 {
     public class TMDBSettings
     {
+        public string BaseUrl { get; set; }
+        public string BaseImagePath { get; set; }
+        public string BaseYouTubePath { get; set; }
+        public QueryOptions QueryOptions { get; set; }
+    }
 
+    public class QueryOptions   
+    {
+        public string Language { get; set; }
+        public string AppendToResponse { get; set; }
+        public string Page { get; set; }
     }
 }
