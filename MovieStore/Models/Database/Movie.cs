9c6fd0a3369c609f7950e7cb562d6e10fa2e9f79
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-26 09:42:20 +0100

    add Movie Model

---
+++ MovieStore/Models/Database/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+using MovieStore.Enums;
+
+namespace MovieStore.Models.Database
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string TagLine { get; set; }
+        public string Overview { get; set; }
+        public int RunTime { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        public MovieRating Rating { get; set; }
+
+        public float RateAverage { get; set; }
+        
+        public byte[] Poster { get; set; }
+        public string PosterType { get; set; }
+        public byte[] BackDrop { get; set; }
+        public string BackdropType { get; set; }
+        public string TrailerUrl { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Poster Image")]
+        public IFormFile PosterFile { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Backdrop Image")]
+        public IFormFile BackdropFile { get; set; }
+    }
+}
