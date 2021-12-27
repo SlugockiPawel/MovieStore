7c87af6851606718e8e60920abc4a4624bfc7703
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-27 17:33:21 +0100

    add LandingPage View Model

---
+++ MovieStore/Models/ViewModels/LandingPageVM.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using MovieStore.Models.Database;
+using MovieStore.Models.TMDB;
+
+namespace MovieStore.Models.ViewModels
+{
+    public class LandingPageVM
+    {
+        public List<Collection> CustomCollections { get; set; }
+        public MovieSearch NowPlaying { get; set; }
+        public MovieSearch Popular { get; set; }
+        public MovieSearch TopRated { get; set; }
+        public MovieSearch Upcoming { get; set; }
+    }
+}
