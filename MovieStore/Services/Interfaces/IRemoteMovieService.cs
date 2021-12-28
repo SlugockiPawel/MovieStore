b826c745abf4f75c9f7bc77df345019616d5c516
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-28 17:34:59 +0100

    add IRemoteMovieService

---
+++ MovieStore/Services/Interfaces/IRemoteMovieService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using MovieStore.Enums;
+using MovieStore.Models.TMDB;
+
+namespace MovieStore.Services.Interfaces
+{
+    public interface IRemoteMovieService
+    {
+        Task<MovieDetail> MovieDetailAsync(int id);
+        Task<MovieSearch> SearchMovieAsync(MovieCategory category, int count);
+        Task<ActorDetail> ActorDetailAsync(int id);
+    }
+}
