261376cb580f417c086b82932f3809a2845f465d
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-28 17:46:00 +0100

    add IImageService

---
+++ MovieStore/Services/Interfaces/IImageService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace MovieStore.Services.Interfaces
+{
+    public interface IImageService
+    {
+        Task<byte[]> EncodeImageAsync(IFormFile poster);
+        Task<byte[]> EncodeImageUrlAsync(string imageUrl);
+        string DecodeImage(byte[] poster, string contentType);
+    }
+}
