b1b86f8e731f79b1b6fea1b1ea9b10e9405e3b48
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-28 17:42:42 +0100

    add IDataMappingService

---
+++ MovieStore/Services/Interfaces/IDataMappingService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using MovieStore.Models.Database;
+using MovieStore.Models.TMDB;
+
+namespace MovieStore.Services.Interfaces
+{
+    public interface IDataMappingService
+    {
+        Task<Movie> MapMovieDetailAsync(MovieDetail movie);
+        ActorDetail MapActorDetail(ActorDetail actor);
+    }
+}
