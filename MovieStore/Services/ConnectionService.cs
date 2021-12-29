67f115055bb204248ed7a4e51ac66fb8b57edf28
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-29 18:02:50 +0100

    Add ConnectionService
    it will return connection string based on where our app is running (development-local or production- e.g. Heroku)

---
+++ MovieStore/Services/ConnectionService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace MovieStore.Services
+{
+    public static class ConnectionService
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Postgres");
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            return string.IsNullOrWhiteSpace(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+        }
+
+        private static string BuildConnectionString(string databaseUrl)
+        {
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+            var builder = new NpgsqlConnectionStringBuilder()
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true
+            };
+
+            return builder.ToString();
+        }
+    }
+}
