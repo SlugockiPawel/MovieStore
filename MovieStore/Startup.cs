f3478ab801558f789bcfc6e0ec3b9f96d3b16efb
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-30 16:29:22 +0100

    register SeedService

--- MovieStore/Startup.cs
+++ MovieStore/Startup.cs
@@ -48,6 +48,8 @@ namespace MovieStore
             // services.AddRazorPages();
 
             services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+            services.AddTransient<SeedService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
