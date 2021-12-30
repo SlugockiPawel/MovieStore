c0207260e999cae076ccbc33ad477056afd5d86e
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-30 16:19:47 +0100

    add SeedService class

---
+++ MovieStore/Services/SeedService.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MovieStore.Data;
+using MovieStore.Models.Database;
+using MovieStore.Models.Settings;
+
+namespace MovieStore.Services
+{
+    public class SeedService
+    {
+    }
+}
