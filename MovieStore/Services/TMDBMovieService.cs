43cb7a20c002c4ba7e54e1663ec689e03cad66a1
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-31 11:36:57 +0100

    add TMDBMovieService() with needed dependencies

---
+++ MovieStore/Services/TMDBMovieService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MovieStore.Enums;
+using MovieStore.Models.Settings;
+using MovieStore.Models.TMDB;
+using MovieStore.Services.Interfaces;
+
+namespace MovieStore.Services
+{
+    public class TMDBMovieService : IRemoteMovieService
+    {
+        private readonly AppSettings _appSettings;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TMDBMovieService(IOptions<AppSettings> appSettings, IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _appSettings = appSettings.Value;
+        }
+
+        public Task<MovieDetail> MovieDetailAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<MovieSearch> SearchMovieAsync(MovieCategory category, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ActorDetail> ActorDetailAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
