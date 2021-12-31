8fc22bbe407fd63ef854b7dddb9dfeeb0bc82de5
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-31 20:16:19 +0100

    add TMDBMappingService

---
+++ MovieStore/Services/TMDBMappingService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using MovieStore.Models.Database;
+using MovieStore.Models.TMDB;
+using MovieStore.Services.Interfaces;
+
+namespace MovieStore.Services
+{
+    public class TMDBMappingService : IDataMappingService
+    {
+        public Task<Movie> MapMovieDetailAsync(MovieDetail movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ActorDetail MapActorDetail(ActorDetail actor)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
