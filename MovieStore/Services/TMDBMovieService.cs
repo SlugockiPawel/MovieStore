f3cc2e64683023d52d64bc75f8e56c640208cc55
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-12-31 12:54:26 +0100

    implement SearchMovieAsync()

--- MovieStore/Services/TMDBMovieService.cs
+++ MovieStore/Services/TMDBMovieService.cs
@@ -1,5 +1,9 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Options;
 using MovieStore.Enums;
 using MovieStore.Models.Settings;
@@ -24,9 +28,39 @@ namespace MovieStore.Services
             throw new System.NotImplementedException();
         }
 
-        public Task<MovieSearch> SearchMovieAsync(MovieCategory category, int count)
+        public async Task<MovieSearch> SearchMovieAsync(MovieCategory category, int count)
         {
-            throw new System.NotImplementedException();
+            // Setup a default instance of MovieSearch
+            MovieSearch movieSearch = new();
+
+            // Assemble full request uri string
+            var query = $"{_appSettings.TMDBSettings.BaseUrl}/movie/{category}";
+
+            var queryParams = new Dictionary<string, string>()
+            {
+                { "api_key", _appSettings.MovieStoreSettings.TmDbApiKey },
+                { "language", _appSettings.TMDBSettings.QueryOptions.Language },
+                { "page", _appSettings.TMDBSettings.QueryOptions.Page }
+            };
+
+            var requestUri = QueryHelpers.AddQueryString(query, queryParams);
+
+            // Create a client and execute the request
+            var client = _httpClientFactory.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            var response = await client.SendAsync(request);
+
+            // Return MovieSearch object
+            if (response.IsSuccessStatusCode)
+            {
+                var dcjs = new DataContractJsonSerializer(typeof(MovieSearch));
+                await using var responseStream = await response.Content.ReadAsStreamAsync();
+                movieSearch = (MovieSearch)dcjs.ReadObject(responseStream);
+                movieSearch.results = movieSearch.results.Take(count).ToArray();
+                movieSearch.results.ToList().ForEach(r => r.poster_path = $"{_appSettings.TMDBSettings.BaseImagePath}/{_appSettings.MovieStoreSettings.DefaultPosterSize}/{r.poster_path}");
+            }
+
+            return movieSearch;
         }
 
         public Task<ActorDetail> ActorDetailAsync(int id)
