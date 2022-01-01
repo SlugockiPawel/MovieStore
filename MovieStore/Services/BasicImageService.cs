8d16d421e222643a58e540ef2cf389143a0f9ba9
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-01-01 10:51:10 +0100

    add BasicImageService class

---
+++ MovieStore/Services/BasicImageService.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using MovieStore.Services.Interfaces;
+
+namespace MovieStore.Services
+{
+    public class BasicImageService : IImageService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BasicImageService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public Task<byte[]> EncodeImageAsync(IFormFile poster)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<byte[]> EncodeImageUrlAsync(string imageUrl)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string DecodeImage(byte[] poster, string contentType)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
