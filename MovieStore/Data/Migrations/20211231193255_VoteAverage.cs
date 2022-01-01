3241dc63f0d194cbfaeb4d4fba2a67034d3d3e30
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-01-01 10:00:44 +0100

    add migration to reflect property name change

---
+++ MovieStore/Data/Migrations/20211231193255_VoteAverage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieStore.Data.Migrations
+{
+    public partial class VoteAverage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RateAverage",
+                table: "Movie",
+                newName: "VoteAverage");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "VoteAverage",
+                table: "Movie",
+                newName: "RateAverage");
+        }
+    }
+}
