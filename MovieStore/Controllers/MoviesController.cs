f9429e809ccc3e25084aec069b96d11e8b50ab40
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-01-02 13:02:37 +0100

    implement Import POST Action

--- MovieStore/Controllers/MoviesController.cs
+++ MovieStore/Controllers/MoviesController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MovieStore.Data;
+using MovieStore.Models.Database;
+using MovieStore.Models.Settings;
+using MovieStore.Services.Interfaces;
 
 namespace MovieStore.Controllers
 {
     public class MoviesController : Controller
     {
-        public IActionResult Index()
         private readonly AppSettings _appSettings;
         private readonly ApplicationDbContext _context;
         private readonly IImageService _imageService;
@@ -25,6 +32,33 @@ namespace MovieStore.Controllers
         {
             var movies = await _context.Movies.ToListAsync();
             return View(movies);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Import(int id)
+        {
+            // if we already have this movie we can just warn the user instead of importing it again
+            if (_context.Movies.Any(m => m.MovieId == id))
+            {
+                var localMovie = await _context.Movies.FirstOrDefaultAsync(m => m.MovieId == id);
+                return RedirectToAction("Details", "Movies", new { id = localMovie.Id, local = true });
+            }
+
+            // If there is no such movie in the db, get raw data from the API
+            var movieDetail = await _tmdbMovieService.MovieDetailAsync(id);
+
+            // Run the data through the mapping procedure
+            var movie = await _tmdbMappingService.MapMovieDetailAsync(movieDetail);
+
+            // Add movie to the database
+            await _context.Movies.AddAsync(movie); // _context.Add(movie) ?  TODO CHECK WHAT IS A DIFFERENCE!
+            await _context.SaveChangesAsync();
+
+            // Assign the movie to the default 'All' collection
+            await AddToMovieCollection(movie.Id, _appSettings.MovieStoreSettings.DefaultCollection.Name);
+
+            return RedirectToAction("Import");
         }
 
         private async Task AddToMovieCollection(int movieId, string collectionName)
