8c085ca90c2de03faae4ea301c0bb90b3c4aef27
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-01-02 13:00:27 +0100

    add needed services via constructor injection

--- MovieStore/Controllers/MoviesController.cs
+++ MovieStore/Controllers/MoviesController.cs
@@ -5,6 +5,22 @@ namespace MovieStore.Controllers
     public class MoviesController : Controller
     {
         public IActionResult Index()
+        private readonly AppSettings _appSettings;
+        private readonly ApplicationDbContext _context;
+        private readonly IImageService _imageService;
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _tmdbMappingService;
+
+        public MoviesController(IOptions<AppSettings> appSettings, ApplicationDbContext context, IImageService imageService,
+            IRemoteMovieService tmdbMovieService, IDataMappingService tmdbMappingService)
+        {
+            _appSettings = appSettings.Value;
+            _context = context;
+            _imageService = imageService;
+            _tmdbMovieService = tmdbMovieService;
+            _tmdbMappingService = tmdbMappingService;
+        }
+
         {
             return View();
         }
