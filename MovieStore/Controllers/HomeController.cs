5e715b0f70569b31d05b0f229f6af4876c2f1b30
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-01-02 12:02:39 +0100

    implement Index Action

--- MovieStore/Controllers/HomeController.cs
+++ MovieStore/Controllers/HomeController.cs
@@ -6,21 +6,44 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieStore.Data;
+using MovieStore.Enums;
+using MovieStore.Models.ViewModels;
+using MovieStore.Services.Interfaces;
 
 namespace MovieStore.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IRemoteMovieService _tmdMovieService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, IRemoteMovieService tmdMovieService)
         {
             _logger = logger;
+            _context = context;
+            _tmdMovieService = tmdMovieService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            const int count = 16;
+            var data = new LandingPageVM()
+            {
+                CustomCollections = await _context.Collections
+                    .Include(c => c.MovieCollections)
+                    .ThenInclude(mc => mc.Movie)
+                    .ToListAsync(),
+
+                NowPlaying = await _tmdMovieService.SearchMovieAsync(MovieCategory.now_playing, count),
+                Popular = await _tmdMovieService.SearchMovieAsync(MovieCategory.popular, count),
+                TopRated = await _tmdMovieService.SearchMovieAsync(MovieCategory.top_rated, count),
+                Upcoming = await _tmdMovieService.SearchMovieAsync(MovieCategory.upcoming, count),
+            };
+
+            return View(data);
         }
 
         public IActionResult Privacy()
