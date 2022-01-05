8991a9bb42a593999d053e05ea0add62ec897677
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-01-05 14:10:11 +0100

    add MovieCollectionsController

---
+++ MovieStore/Controllers/MovieCollectionsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStore.Data;
+
+namespace MovieStore.Controllers
+{
+    public class MovieCollectionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MovieCollectionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
