c7467564f980c74dc9f87bd81d14b0cb2c23d06f
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-01-05 15:03:59 +0100

    add ActorsController

---
+++ MovieStore/Controllers/ActorsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStore.Services.Interfaces;
+
+namespace MovieStore.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _mappingService;
+
+        public ActorsController(IRemoteMovieService tmdbMovieService, IDataMappingService mappingService)
+        {
+            _tmdbMovieService = tmdbMovieService;
+            _mappingService = mappingService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
