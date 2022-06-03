c0288335fdd7cd84c27020e8833675c837a474ea
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-06-03 20:08:28 +0200

    fix snapshot and migrations files to properly change BackDrop column name to Backdrop
    Movie model file

---
+++ MovieStore/Data/Migrations/20220603180530_fixBackdropName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieStore.Data.Migrations
+{
+    public partial class fixBackdropName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BackDrop",
+                table: "Movies",
+                newName: "Backdrop");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Backdrop",
+                table: "Movies",
+                newName: "BackDrop");
+        }
+    }
+}
